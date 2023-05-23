2bdcb1007d3d536699b363a22ce5f3ddb16de5d8
Heizel Gonzalez <34485761+gr8lady@users.noreply.github.com>
2023-05-23 13:08:39 -0600

    Create helloWorld.php
    creating a basic php page

---
+++ helloWorld.php
@@ -0,0 +1,16 @@
+<!DOCTYPPE html>
+<html>
+    <head> Integrating php and HTML</head>
+<body>
+    <h1> welcome to php</h1>
+    <!--inline php block-->
+    <p>Today is <?php print( date("l F d, Y"))?></p>
+    <h2>PHP block</h2>
+     <!--multiline php block-->
+     <?php
+        print("<p> my PHP code samples </p>");
+        print("<p> <strong>Heizel G</strong> </p>");
+     ?>
+    
+</body>
+</html>
