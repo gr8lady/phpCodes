1ad81558ae8556049ce11b704e28ba5a0c00dcf6
Heizel Gonzalez <heizel@gmail.com>
2023-05-23 14:05:34 -0600

    updating code constant and variables

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+// user defined constants
+define("SCREENWITHD", 1920);
+define("SCREENHEIGHT", 1080);
+define("MODE", "2D");
+
+print("Width:" . SCREENWITHD);
+print("<br />height: ". SCREENHEIGHT);
+
+// magic contstants
+print("<br /> File Name: ". __FILE__ );
+
+?>
